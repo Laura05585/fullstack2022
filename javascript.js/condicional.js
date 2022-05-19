78777372e6f7b38c3742644e6ea05c07c845b016
Laura05585 <105198525+Laura05585@users.noreply.github.com>
2022-05-19 11:08:50 +0200

    c

--- javascript.js/condicional.js
+++ javascript.js/condicional.js
@@ -1,4 +1,15 @@
-consta=4; 
-constb=2;
-if(a>b)
-{console.log(`a(${a})esmayorqueb (${b})`);}Puedene
+const a = 3;
+const b = 5;
+if (a > b) {
+    console.log(`a (${a}) es mayor que b (${b})`);
+    document.write("condición 1 a>b");
+} else if (a < b) {
+    console.log(`b (${b}) es mayor que a (${a})`);
+    document.write("a < b");
+} else if (a === b) {
+    console.log(`a (${a}) es igual y posee el mismo tipo que b (${b})`);
+    document.write("a=b valor y tipo");
+} else {
+    console.log(`ELSE a (${a}) es igual en valor que b (${b})`);
+    document.write("ELSE a=b solo en valor");
+}
--- javascript.js/horarioslogicos.js
+++ javascript.js/horarioslogicos.js
@@ -1,9 +1,8 @@
-let hora=10;
-if ((hora>=9) && (hora<=14)){
-    document.write ("sant boi");
-} 
-else if (hora>=15) && (hora<=20){
-    document.write ("castelldefels");
-} else  {
-    document.write ("no estoy en clase");
+let hora= 21;
+if ((hora>=9) && (hora<=14)) {
+    document.write("Sant Boi");
+} else if ((hora>=15) && (hora<=20)) {
+    document.write("Castelldefels");
+} else {
+    document.write("No estoy en clase");
 }
