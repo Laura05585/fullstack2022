c586fa464ba7409da92bb7e14b28e38fa3b22f6e
Laura05585 <105198525+Laura05585@users.noreply.github.com>
2022-05-23 13:27:22 +0200

    c

--- javascript.js/ejercicios strings.js
+++ javascript.js/ejercicios strings.js
@@ -4,8 +4,14 @@
 let ejercicio3= "abc 1 abc a abc";
 let existeABC = ejercicio3.indexOf("abc");
 // cumple que existe del 0 al 14 pero repasemos donde 
-if (condition) {
+if (ejercicio3.indexOf("abc") !=-1) && 
+(ejercicio3={
     console.log ("existe abc dentro del string");
 } else {
     console.log("no existe ABC dentro");
 }
+
+//Ejercicio4:escribeunprograma quedadounstringyunadeterminadoposición, compruebequeel carácteranteriory elposteriorson igualeso no.
+let stringsPos 
+
+//Ejercicio5:escribeunprograma quemuestreporpantallalaposicióndelasegunday tercera ocurrencia del carácter'a' en un stringdado.Ejercicio6:escribeunprograma quedevuelva siunstringespalíndromo(seescribe igual haciadelante yhacia detrás). Ejemplo:"sometemos".Ejercicio7:escribeunprogramaqueelimineelúltimocarácterdeunstring.Ejercicio8:escribeunprograma queinserteelcarácter'b'cadatresposicionesenun string.Ejercicio 9: escribe un programaque conviertaen mayúsculasla primeraletrade cada palabra de un string.
--- javascript.js/string-javascript.js
+++ javascript.js/string-javascript.js
@@ -1,11 +1,10 @@
-cons tstr ='En un lugar de la mancha';
-cons tstr2 ='En otro lugar';
-const ultimoString1= str1.length-1; // guarda 23
-const ultimoString1= str2.length-1; // guarda 12
+const str1 ="En un lugar de la mancha";
+const str2 ="En otro lugar";
+const u1= str1.length-1; // Guarda 23
+const u2= str2.length-1; // Guarda 12
 
-if ((str1.chartAt (0) == str2.charAt(0)) && (str1.
-    charAt (u1) == str2.charAt(u2))) {
-    document.write("los dos string tienen la letra igual")
-    } else {
-    document.write("tienen la primer letra diferente")
-    }
+if ((str1.charAt(0) == str2.charAt(0)) && (str1.charAt(u1) == str2.charAt(u2))) {
+    document.write("Los dos strings tienen la primera letra y la última igual");
+} else {
+    document.write("Tienen la primera o la última letra diferente");
+}
