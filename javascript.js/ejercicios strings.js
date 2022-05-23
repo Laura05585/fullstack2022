2a6eba4ec92bc5a65ec277042e25c083596872b7
Laura05585 <105198525+Laura05585@users.noreply.github.com>
2022-05-23 12:33:57 +0200

    c

---
+++ javascript.js/ejercicios strings.js
@@ -0,0 +1,11 @@
+//ejercicio 3 pag 204
+//Ejercicio3:escribeunprogramaquecompruebequeelsubstring"abc"existeenun stringdado,pero no puedeencontrarseni al comienzo, ni al final.
+
+let ejercicio3= "abc 1 abc a abc";
+let existeABC = ejercicio3.indexOf("abc");
+// cumple que existe del 0 al 14 pero repasemos donde 
+if (condition) {
+    console.log ("existe abc dentro del string");
+} else {
+    console.log("no existe ABC dentro");
+}
---
+++ javascript.js/string-javascript.js
@@ -0,0 +1,11 @@
+cons tstr ='En un lugar de la mancha';
+cons tstr2 ='En otro lugar';
+const ultimoString1= str1.length-1; // guarda 23
+const ultimoString1= str2.length-1; // guarda 12
+
+if ((str1.chartAt (0) == str2.charAt(0)) && (str1.
+    charAt (u1) == str2.charAt(u2))) {
+    document.write("los dos string tienen la letra igual")
+    } else {
+    document.write("tienen la primer letra diferente")
+    }
