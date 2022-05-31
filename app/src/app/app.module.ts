d9275d458ad629bf51944ea8325334e1a682e2cc
Laura05585 <105198525+Laura05585@users.noreply.github.com>
2022-05-31 12:47:55 +0200

    c

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -2,15 +2,56 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes/heroes.component';
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+app.module.ts (@NgModule imports)
+content_copy
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+imports: [
+  BrowserModule,
+  FormsModule
+],
+import { HeroesComponent } from './heroes/heroes.component';
+import { AppRoutingModule } from './heroes/app-routing.module';
+declarations: [
+  AppComponent,
+  HeroesComponent
+],
+
